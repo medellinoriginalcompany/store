a520915982c730cde3da9adbd569cf7cc7e8bcf8
Vini <vsgh-gouveia@hotmail.com>
2024-11-14 18:24:20 -0300

    Initial commit

---
+++ src/api/hello-world/route.ts
@@ -0,0 +1,15 @@
+import type {
+  MedusaRequest,
+  MedusaResponse,
+} from "@medusajs/framework/http"
+
+
+export const GET = (
+  req: MedusaRequest,
+  res: MedusaResponse
+) => {
+
+  res.json({
+    message: "Hello, world!",
+  })
+}
